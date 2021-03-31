8f645badc549bc13a691ec0e7887139de38ca364
Daniel <dannyblaze469@gmail.com>
2021-03-30 20:40:46 -0400

    Finished my attr accessor

--- ActiveRecordsP1/lib/00_attr_accessor_object.rb
+++ ActiveRecordsP1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,26 @@
+require "byebug"
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+
+      define_method(name) do
+        # debugger
+        instance_variable_get("@#{name}")
+      end
+      define_method("#{name}=") do |val| 
+        instance_variable_set("@#{name}", val)
+      end
+
+    end
   end
+    my_attr_accessor(:x, :y)
 end
+
+
+#  define_method(attr) { instance_variable_get("@#{attr}") }
+      
+#  define_method("#{attr}=") { |val| instance_variable_set
+#       ("@#{attr}", val) }
+# def x=(val)
+#   @x = val
+# end
